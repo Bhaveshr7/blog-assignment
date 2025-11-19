9c989216f0ef699bdc8b544a1871f6f614caf910
Rohith <rohithsakthit@gmail.com>
2025-11-20 02:14:42 +0530

    Updated header

--- components/Header.tsx
+++ components/Header.tsx
@@ -4,7 +4,7 @@ export default function Header() {
   return (
     <header className="bg-gray-200 shadow-sm sticky top-0 z-50">
       <div className="max-w-6xl mx-auto px-4 sm:px-6 lg:px-8 flex items-center justify-between h-16">
-        <Link href="/" className="text-2xl font-bold">Recipes Blog</Link>
+        <Link href="/" className="text-2xl font-bold">Posts Blog</Link>
 
         <nav className="space-x-4">
           <Link href="/about" className="text-sm hover:text-pink-600">About</Link>
